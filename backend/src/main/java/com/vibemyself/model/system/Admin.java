72c71e3f9dc6df3d7c8a217d3416a0b50e1be4e9
나윤주 <nyj@NYJui-MacBookPro.local>
2026-03-25 10:29:12 +0900

    feat: add Admin domain model

---
+++ backend/src/main/java/com/vibemyself/model/system/Admin.java
@@ -0,0 +1,18 @@
+package com.vibemyself.model.system;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+    private String loginId;
+    private String loginPwd;
+    private String adminNm;
+    private String roleCd;
+    private String useYn;
+}
